e2458f887ef471ae86b2c36ce7fa85fdf040c4ef
Luis Felipe Valadares Pantoja <89617375+FelipeLuis123@users.noreply.github.com>
2023-07-10 22:59:43 -0300

    wsgi

--- recomendacao/recomendacao/wsgi.py
+++ recomendacao/recomendacao/wsgi.py
@@ -9,5 +9,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling, MediaCling
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recomendacao.settings')
 application = Cling(MediaCling(get_wsgi_application()))
