53e2e30434550213f2efc9654a6abb34beec0f72
Luis Felipe Valadares Pantoja <89617375+FelipeLuis123@users.noreply.github.com>
2023-07-16 17:46:26 -0300

    foi adicionado duas imagens para o footer e colocado na tela principal.

--- recomendacao/app/urls.py
+++ recomendacao/app/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-
 from . import views
 urlpatterns = [
-    path('', views.login, name = 'login'),
+    path('', views.login, name='login'),
     path('login', views.login, name='login'),
     path('cadastro', views.cadastro, name='cadastro'),
     path('principal', views.principal, name='principal'),
     path('cadrm', views.cadrm, name='cadrm'),
-
 ]
